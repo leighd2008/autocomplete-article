91a0ca77a2efe79929d97611da427f5d75108525
Diane Leigh <leighd2008@gmail.com>
2023-06-17 08:41:14 -0400

    testing downshift

--- src/App.js
+++ src/App.js
@@ -1,156 +1,33 @@
 import React, { useState } from 'react';
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 
+import "./styles.css";
+import ButtonsResult from "./components/ButtonsResult";
+import DownShift from "./components/DownShift";
+
+const defaultValues = {
+  downShift: "apple"
+}
 
 function App() {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const { register, handleSubmit } = useForm();
+  const { handleSubmit, register, reset, control } = useForm({ defaultValues });
+  const [data, setData] = useState(null);
   
-  
-  // *************** For Autocomplete ***************
-  const [activeSuggestion, setActiveSuggestion] = useState(0)
-  const [filteredSuggestions, setFilteredSuggestions] = useState([])
-  const [showSuggestions, setShowSuggestions] = useState(false)
-  const [userInput, setUserInput] = useState('')
-  
-  const suggestions = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District Of Columbia", "Federated States Of Micronesia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Marshall Islands", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Palau", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Islands", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-    
-  
-  const onChange = e => {
-    setUserInput(e.currentTarget.value)
-    
-    setFilteredSuggestions(suggestions.filter(
-      suggestion => 
-        suggestion.toLowerCase().startsWith(e.currentTarget.value.toLowerCase())
-    ))
-      setActiveSuggestion(0)
-      setShowSuggestions(true)
-  };
-  
-  const onClick = e => {
-      setActiveSuggestion(0)
-      setFilteredSuggestions([])
-      setShowSuggestions(false)
-      setUserInput(e.currentTarget.innerText)
-  };
-  
-  const onKeyDown = e => {
-    if (e.keyCode === 13 || e.keyCode === 9) {
-      setActiveSuggestion(0)
-      setShowSuggestions(false)
-      setUserInput(filteredSuggestions[activeSuggestion])
-    } else if (e.keyCode === 38) {
-      if (activeSuggestion === 0) {
-        return;
-      }
-      setActiveSuggestion(activeSuggestion - 1)
-    }
-    // User pressed the down arrow, increment the index
-    else if (e.keyCode === 40) {
-      if(activeSuggestion - 1 === filteredSuggestions.length) {
-        return;
-      }
-      setActiveSuggestion(activeSuggestion + 1)
-    }
-  };
-    
-    let suggestionListComponent;
-    
-    if (showSuggestions && userInput) {
-      if (filteredSuggestions.length) {
-        suggestionListComponent = (
-          <ul className="suggestions">
-            {filteredSuggestions.map((suggestion, index) => {
-              let className;
-              
-              if (index === activeSuggestion) {
-                className = "suggestion-active";
-              }
-              return (
-                <li className={className} key={suggestion} onClick={onClick}>
-                  {suggestion}
-                </li>
-              );
-            })}
-          </ul>
-        );
-      } else {
-        suggestionListComponent = (
-          <div className="no-suggestions">
-            <em>No suggestions available.</em>
-          </div>
-        );
-      }
-    }
-    
-  // *************** For Autocomplete ***************
-    
-  const onFirstNameChanged = e => setFirstName(e.target.value)
-  const onLastNameChanged = e => setLastName(e.target.value)
-  
-  const canSave = [firstName, lastName].every(Boolean)
-  
-  const onSubmit = async (data) => {
-    
-    if (canSave) {
-      console.log(data)
-    }
-  }
-
   return (
-    <section className="section">
-      <div className="centered-view">
-        <section className="centered-container">
-          <div className="">
-            <form onSubmit={handleSubmit(onSubmit)} className="centered-container-form">
-              <div className="header">New Client Information</div>
-              <div className="form-container">
-                <div className="form-group">
-                  <label htmlFor="firstName">First Name</label>
-                  <input
-                    {...register('firstName')}
-                    type="text"
-                    className="form-control"
-                    required={true}
-                    id="firstName"
-                    name="firstName"
-                    onChange={onFirstNameChanged}
-                    />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="last name" >Last Name</label>
-                  <input
-                    {...register('lastName')}
-                    type="text"
-                    className="form-control"
-                    required={true}
-                    id="lastName"
-                    name="lastName"
-                    onChange={onLastNameChanged}
-                    />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="usState" >State</label>
-                  <input
-                    {...register('usState')}
-                    type="text"
-                    className="form-control"
-                    id="usState"
-                    name="usState"
-                    onChange={onChange}
-                    onKeyDown={onKeyDown}
-                    value={userInput}
-                    />
-                    {suggestionListComponent}
-                </div>
-                <button type="submit" className="btn">Add Client</button>
-              </div>
-            </form>
-          </div>
+    <form onSubmit={handleSubmit(data => setData(data))} className="form" >
+      <div className='container'>
+        <section>
+          <label >Native Input: </label>
+          <input name='Native' className='input' {...register('native')} />
+        </section>
+        <section>
+          <Controller render={DownShift} control={control} name='downShift' />
         </section>
       </div>
-    </section>
+      <ButtonsResult {...{data, reset, defaultValues }} />
+    </form>
   )
+  
+
 }
 export default App;
---
+++ src/components/ButtonsResult.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ data, reset, defaultValues }) => (
+  <>
+    {data && (
+      <pre style={{ textAlign: "left", color: "white" }}>
+        {JSON.stringify(data, null, 2)}
+      </pre>
+    )}
+    
+    <button 
+      className="button buttonBlack"
+      type="button"
+      onClick={() => {
+        reset(defaultValues);
+      }}
+    >
+      Reset Form
+    </button>
+    <button className="button">submit</button>
+  </>
+)
